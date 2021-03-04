09640c1a1366a22682c7e969cd3fa49ec2c3a62c
nfgrep <nfg2600@gmail.com>
2021-03-04 13:08:36 -0500

    added some dealt cards and counters for the suit and face of each card

--- hand-parser.py
+++ hand-parser.py
@@ -11,3 +11,24 @@ class Card:
     return self.face + ":" + self.suit
   def __repr__(self):
     return str(self)
+
+# The 5 cards that we are considering 
+cards_dealt = [Card('2','d'),Card('A','d'),Card('2','s'),Card('A','d'),Card('A','d')]
+
+face_counters = {'A': 0,
+                 '2': 0,
+                 '3': 0,
+                 '4': 0,
+                 '5': 0,
+                 '6': 0,
+                 '7': 0,
+                 '8': 0,
+                 '9': 0,
+                 'T': 0,
+                 'J': 0,
+                 'K': 0}
+
+suit_counters = {'d': 0,
+                 'h': 0,
+                 's': 0,
+                 'c': 0}
